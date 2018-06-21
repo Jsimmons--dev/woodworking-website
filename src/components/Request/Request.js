5e68661471a8cc055b778b259928e273fe882efa
Curtis Kreidler <kreidler@udel.edu>
2018-06-21 13:59:21 -0400

    very incorrect parallax effect on request

--- src/components/Request/Request.js
+++ src/components/Request/Request.js
@@ -7,9 +7,15 @@ export class Request extends Component {
         return (
             <div id={'request'}>
 
-                <div class='request-blurb'>
+                <div class='request-blurb parallax__layer parallax__layer--base'>
                     <p>Interested in purchasing any of the items above, or commissioning some work?</p>
                     <Link to={'/request'}><button id="request-button">send a request!</button></Link>
+                </div>
+                <div class='parallax__layer parallax__layer--back'>
+                
+                </div>
+                <div class='parallax__layer parallax__layer--deep'>
+                    
                 </div>
 
             </div>
